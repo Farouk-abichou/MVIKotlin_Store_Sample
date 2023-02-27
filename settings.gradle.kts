19d0fa2cee5cae6fc8b71e741b30c6fd47cb32d9
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-02-27 16:35:47 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MVI_Application"
+include(":androidApp")
+include(":shared")
